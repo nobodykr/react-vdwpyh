e4824ab218eb58280e88fbe61c7469f0c821891e
nobodykr <diogovdcandrade@gmail.com>
2022-03-08 21:42:33 +0000

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('main'));
+
+let demo = React.createElement(
+  'h1',
+  { style: { color: 'green' } },
+  'Welcome to GeeksforGeeks'
+);
+ReactDOM.render(demo, document.getElementById('main'));
